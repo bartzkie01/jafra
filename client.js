fd326e01bbc1e116d235cef050d2a7c9a2213076
bartzkie01 <167133793+bartzkie01@users.noreply.github.com>
2024-04-16 04:08:19 +0700

    Update client.js

--- client.js
+++ client.js
@@ -1,31 +1,27 @@
 document.getElementById('loginForm').addEventListener('submit', async (e) => {
-    e.preventDefault();
-    const username = document.getElementById('username').value;
-    const password = document.getElementById('password').value;
-    
-    try {
-        const response = await fetch('/login', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({ username, password })
-        });
+  e.preventDefault();
+  const username = document.getElementById('username').value;
+  const password = document.getElementById('password').value;
 
-        if (!response.ok) {
-            // Handle error response from the server
-            const errorData = await response.json();
-            document.getElementById('message').innerText = errorData.error;
-            return;
-        }
+  try {
+    const response = await fetch('/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ username, password })
+    });
 
-        // If login is successful, redirect to the appropriate page based on the response
-        const responseData = await response.json();
-        const redirectUrl = responseData.redirect;
-        window.location.href = redirectUrl;
-    } catch (error) {
-        // Handle network errors or other unexpected errors
-        console.error('Error:', error);
-        document.getElementById('message').innerText = 'An error occurred. Please try again later.';
+    const responseData = await response.json();
+
+    if (!response.ok) {
+      throw new Error(responseData.error);
     }
+
+    const redirectUrl = responseData.redirect;
+    window.location.href = redirectUrl;
+  } catch (error) {
+    console.error('Error:', error);
+    document.getElementById('message').innerText = error.message || 'An error occurred. Please try again later.';
+  }
 });
